326b8e0c9b5b05510c2ab640682c80fd7fdbbaf6
love2hina <webmaster@love2hina.net>
2025-01-13 16:32:23 +0900

    WORK: extension development

--- launch.sh
+++ launch.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python main.py --input-directory '/mnt/z/Program Files/StableDiffusion/inputs' --output-directory '/mnt/z/Program Files/StableDiffusion/outputs/comfyui'
+python main.py \
+ --port 8188 \
+ --disable-cuda-malloc \
+ --input-directory '/mnt/z/Program Files/StableDiffusion/inputs' \
+ --output-directory '/mnt/z/Program Files/StableDiffusion/outputs/comfyui'
